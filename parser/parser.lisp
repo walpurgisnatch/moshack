a5b872db28bea68382df411189b2ed6e6d2004ab
Walpurgisnatch <Kawemirs@gmail.com>
2022-06-09 17:30:54 +0700

    item parsing

--- parser/parser.lisp
+++ parser/parser.lisp
@@ -6,9 +6,11 @@
 
 (setf x (ss:parse "https://productcenter.ru/producers/r-moskovskaia-obl-191/c-moskva-3109?sorttype=alphabet"))
 
-(ss:collect-from x 'div :test #'(lambda (n a) (equal (ss:attribute n 'class) a)) :test-args '("ci_main"))
 
 (setf producers (ss:extract-urls x #'(lambda (q y) (substp y q)) "/producers/"))
+
+(defun producers-links (page)
+  (ss:extract-urls page #'(lambda (q y) (and (substp y q) (not (substp q "/page-")))) "/producers/"))
 
 (defun from-table (page attr)
   (let ((tds (loop for td in (ss:collect-from page 'tr)
@@ -21,7 +23,7 @@
   (plump:text (ss:node-with-attr page selector attr val)))
 
 (defun parse-organization (page)
-  (let* ((name (ss:text page 'h1))
+  (let ((name (ss:text page 'h1))
          (description (text page 'div 'class "iv_text"))
          (category (plump:text (nth 4 (ss:collect-from page 'li :test (ss:check-attr 'itemtype) :test-args "http://schema.org/ListItem"))))
          (address (text page 'td 'itemprop "address"))
@@ -29,5 +31,11 @@
          (phone (text page 'span 'itemprop "telephone"))
          (inn (from-table page "ИНН")))
     (list name description category address website phone inn)))
+
+(defun parse-item (page)
+  (let ((name (ss:text page 'h1))
+        (description (text page 'div 'class "iv_text"))
+        (price (text page 'div 'class "price")))
+    (list name description price)))
 
 (setf price (ss:text item 'div :test (ss:check-attr 'class) :test-args "price"))))
