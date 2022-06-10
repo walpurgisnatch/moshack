8a0d216ac4f405a91e149067014034c262284322
Walpurgisnatch <Kawemirs@gmail.com>
2022-06-10 12:07:29 +0700

    parser full setup

--- parser/parser.lisp
+++ parser/parser.lisp
@@ -4,13 +4,31 @@
 
 (in-package :moshack.parser)
 
-(setf x (ss:parse "https://productcenter.ru/producers/r-moskovskaia-obl-191/c-moskva-3109?sorttype=alphabet"))
+(defparameter *headers* '(("User-Agent" . "Mozilla/5.0 (X11; Linux x86_64; rv:96.0) Gecko/20100101 Firefox/96.0") ("Cookie . baner_views_70=3; baner_views_71=3; baner_counter_5=1; baner_counter_8=2; items_on_page=48")))
 
+(defparameter *main-link* "https://productcenter.ru/producers/r-moskovskaia-obl-191/c-moskva-3109/page-")
 
-(setf producers (ss:extract-urls x #'(lambda (q y) (substp y q)) "/producers/"))
+(defun substp (regex string)
+  (cl-ppcre:scan-to-strings regex string))
+
+(defun prod-link (string q)
+  (substp "\/producers\/[0-9]*?\/[a-zA-Z]" string))
+
+(defun make-producers-link (page)
+  (concatenate 'string *main-link* (write-to-string page)))
+
+(defun organization-items-link (page)
+  (ss:find-by-text page "Смотреть всё" :attr 'href))
 
 (defun producers-links (page)
-  (ss:extract-urls page #'(lambda (q y) (and (substp y q) (not (substp q "/page-")))) "/producers/"))
+  (remove-duplicates (ss:extract-urls (ss:parse (make-producers-link page)  :headers *headers*) #'prod-link) :test #'string=))
+
+(defun producers (&optional (n 63))
+  (loop for i from 1 to n do (sleep 1) collect (producers-links (ss:parse (concatenate 'string *main-link* (write-to-string i)) :headers *headers*))))
+
+(defun parse-all ()
+  (loop for i from i to n do (sleep 1)
+        do (loop for link in producers-links (ss:parse))))
 
 (defun from-table (page attr)
   (let ((tds (loop for td in (ss:collect-from page 'tr)
@@ -32,10 +50,21 @@
          (inn (from-table page "ИНН")))
     (list name description category address website phone inn)))
 
+(defun organization-items (page)
+  (remove-duplicates
+   (ss:collect-from
+    page 'a
+    :attr 'href
+    :test #'(lambda (node q)
+              (and (substp "\/products\/[0-9]*?\/[a-zA-Z]"
+                           (ss:attribute node 'href))
+                   (not (equal (ss:attribute node 'rel) "nofollow"))
+                   (not (substp "sbb_offer" (ss:attribute node 'class))))))
+   :test #'string=))
+
 (defun parse-item (page)
   (let ((name (ss:text page 'h1))
         (description (text page 'div 'class "iv_text"))
         (price (text page 'div 'class "price")))
     (list name description price)))
 
-(setf price (ss:text item 'div :test (ss:check-attr 'class) :test-args "price"))))
