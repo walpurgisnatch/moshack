6536aa879e28d2977b0f25c24cac169b80e4a54e
Walpurgisnatch <Kawemirs@gmail.com>
2022-06-10 16:26:28 +0700

    parser almost

--- parser/parser.lisp
+++ parser/parser.lisp
@@ -18,17 +18,13 @@
   (concatenate 'string *main-link* (write-to-string page)))
 
 (defun organization-items-link (page)
-  (ss:find-by-text page "Смотреть всё" :attr 'href))
+  (ss:prepare-url (ss:find-by-text page "Смотреть всё" :attr 'href) *main-link*))
 
 (defun producers-links (page)
   (remove-duplicates (ss:extract-urls (ss:parse (make-producers-link page)  :headers *headers*) #'prod-link) :test #'string=))
 
 (defun producers (&optional (n 63))
   (loop for i from 1 to n do (sleep 1) collect (producers-links (ss:parse (concatenate 'string *main-link* (write-to-string i)) :headers *headers*))))
-
-(defun parse-all ()
-  (loop for i from i to n do (sleep 1)
-        do (loop for link in producers-links (ss:parse))))
 
 (defun from-table (page attr)
   (let ((tds (loop for td in (ss:collect-from page 'tr)
@@ -38,33 +34,42 @@
             return (plump:text (car val)))))
 
 (defun text (page selector attr val)
-  (plump:text (ss:node-with-attr page selector attr val)))
+  (handler-case
+      (plump:text (ss:node-with-attr page selector attr val))
+    (error (e) nil)))
+
+(defun organization-items-links (org-page)
+  (let ((page (ss:parse (organization-items-link org-page) :headers *headers*)))
+    (remove-duplicates
+     (ss:collect-from
+      page 'a
+      :attr 'href
+      :test #'(lambda (node q)
+                (and (substp "\/products\/[0-9]*?\/[a-zA-Z]"
+                             (ss:attribute node 'href))
+                     (not (equal (ss:attribute node 'rel) "nofollow"))
+                     (not (substp "sbb_offer" (ss:attribute node 'class))))))
+     :test #'string=)))
 
 (defun parse-organization (page)
   (let ((name (ss:text page 'h1))
-         (description (text page 'div 'class "iv_text"))
-         (category (plump:text (nth 4 (ss:collect-from page 'li :test (ss:check-attr 'itemtype) :test-args "http://schema.org/ListItem"))))
-         (address (text page 'td 'itemprop "address"))
-         (website (text page 'a 'itemprop "url"))
-         (phone (text page 'span 'itemprop "telephone"))
-         (inn (from-table page "ИНН")))
+        (description (text page 'div 'class "iv_text"))
+        (category (plump:text (nth 4 (ss:collect-from page 'li :test (ss:check-attr 'itemtype) :test-args "http://schema.org/ListItem"))))
+        (address (text page 'td 'itemprop "address"))
+        (website (text page 'a 'itemprop "url"))
+        (phone (text page 'span 'itemprop "telephone"))
+        (inn (from-table page "ИНН")))
     (list name description category address website phone inn)))
 
-(defun organization-items (page)
-  (remove-duplicates
-   (ss:collect-from
-    page 'a
-    :attr 'href
-    :test #'(lambda (node q)
-              (and (substp "\/products\/[0-9]*?\/[a-zA-Z]"
-                           (ss:attribute node 'href))
-                   (not (equal (ss:attribute node 'rel) "nofollow"))
-                   (not (substp "sbb_offer" (ss:attribute node 'class))))))
-   :test #'string=))
+(defun parse-items (org)
+  (loop for item in (organization-items-links org) do (sleep .5)
+        collect (parse-item (ss:parse (ss:prepare-url item *main-link*)))))
 
 (defun parse-item (page)
   (let ((name (ss:text page 'h1))
         (description (text page 'div 'class "iv_text"))
+        (category (plump:text (nth 4 (ss:collect-from page 'li :test (ss:check-attr 'itemtype) :test-args "http://schema.org/ListItem"))))
+        (sub-category (plump:text (nth 5 (ss:collect-from page 'li :test (ss:check-attr 'itemtype) :test-args "http://schema.org/ListItem"))))
         (price (text page 'div 'class "price")))
-    (list name description price)))
+    (list name description category sub-category price)))
 
